de8b84a469920553eee906cc9dbe7b163736b672
morisyu <morisyu50@gmail.com>
2022-01-27 23:05:00 +0900

    배열 문제 풀이

---
+++ src/baekjoon/step5/Array_10818.java
@@ -0,0 +1,34 @@
+package baekjoon.step5;
+
+// 최소, 최대
+// 최솟값과 최댓값을 찾는 문제
+
+import java.util.Scanner;
+
+public class Array_10818 {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+		int[] array = new int[N];
+		for (int i = 0; i < N; i++) {
+			array[i] = scanner.nextInt();
+		}
+
+		int max = array[0];
+		int min = array[0];
+
+		for (int i = 0; i < N; i++) {
+			if (max < array[i]) {
+				max = array[i];
+			}
+			if (min > array[i]) {
+				min = array[i];
+			}
+		}
+		System.out.println(min + " " + max);
+
+	}
+}
