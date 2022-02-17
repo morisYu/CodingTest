a49d64eb198b4ee802bb2817a8e28801a92e2446
morisYu <morisyu50@gmail.com>
2022-02-17 11:40:43 +0900

    백준 7단계 문자열

---
+++ src/baekjoon/step7/String_2675.java
@@ -0,0 +1,27 @@
+package baekjoon.step7;
+
+// 문자열 반복
+// 각 문자를 반복하여 출력하는 문제
+
+import java.util.Scanner;
+
+public class String_2675 {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		int T = scanner.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int R = scanner.nextInt();
+			String S = scanner.next();
+			char[] ch = S.toCharArray();
+			for (int j = 0; j < ch.length; j++) {
+				for (int k = 0; k < R; k++) {
+					System.out.print(ch[j]);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
