533a8e64e2c79af2df4480d33f80326d35bc794d
morisYu <morisyu50@gmail.com>
2022-02-25 16:06:02 +0900

    백준 8단계 기본수학1

---
+++ src/baekjoon/step8/Math_10757.java
@@ -0,0 +1,111 @@
+package baekjoon.step8;
+
+//큰 수 A+B
+//크기가 큰 정수 A+B를 입력받은 다음, A+B를 출력하는 프로그램을 작성
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Math_10757 {
+	public static void main(String[] args) throws IOException {
+
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//		String argA = st.nextToken();
+//		String argB = st.nextToken();
+//
+//		System.out.println(method2(argA, argB));
+
+		testMath_10757.testCase();
+	}
+
+	// BigInteger 클래스 사용
+	static String method1(String argA, String argB) {
+		String result = null;
+
+		BigInteger A = new BigInteger(argA);
+		BigInteger B = new BigInteger(argB);
+
+		result = A.add(B).toString();
+
+		return result;
+	}
+
+	// 배열 사용
+	static String method2(String argA, String argB) {
+		String result = null;
+
+		char[] chA = argA.toCharArray();
+		char[] chB = argB.toCharArray();
+
+		int length;
+
+		if (chA.length >= chB.length) {
+			length = chA.length + 1;
+		} else {
+			length = chB.length + 1;
+		}
+
+		int[] changeArrayA = new int[chA.length];
+		int[] changeArrayB = new int[chB.length];
+
+		int[] arrayA = new int[length];
+		int[] arrayB = new int[length];
+
+		for (int i = 0; i < chA.length; i++) {
+			changeArrayA[i] = Character.getNumericValue(chA[chA.length - i - 1]);
+			arrayA[i] = changeArrayA[i];
+		}
+
+		for (int i = 0; i < chB.length; i++) {
+			changeArrayB[i] = Character.getNumericValue(chB[chB.length - i - 1]);
+			arrayB[i] = changeArrayB[i];
+		}
+
+//		for (int i = 0; i < length; i++) {
+//			System.out.println(arrayA[i] + " - " + arrayB[i]);
+//		}
+
+		int[] sum = new int[length];
+
+		for (int k = 0; k < length; k++) {
+			sum[k] += arrayA[k] + arrayB[k];
+			if (sum[k] >= 10) {
+				sum[k] -= 10;
+				sum[k + 1] += 1;
+			}
+		}
+
+		int[] changeSum = new int[length];
+		for (int i = 0; i < length; i++) {
+			changeSum[i] = sum[length - i - 1];
+		}
+		result = Arrays.toString(changeSum).replaceAll("[^0-9]","");
+		if(result.charAt(0)=='0') {
+			result=result.substring(1);
+		}
+		return result;
+	}
+}
+
+// 테스트코드
+class testMath_10757 {
+	static void testCase() {
+		int A = (int) (Math.random() * 10000000);
+		int B = (int) (Math.random() * 10000000);
+
+		String argA = Integer.toString(A);
+		String argB = Integer.toString(B);
+
+		System.out.println("argA = " + argA);
+		System.out.println("argB = " + argB);
+
+		System.out.println(Math_10757.method2(argA, argB));
+
+	}
+}
---
+++ src/baekjoon/step8/Math_2839.java
@@ -0,0 +1,63 @@
+package baekjoon.step8;
+
+// 설탕 배달
+// 5와 3을 최소 횟수로 합하여 N을 만드는 문제
+
+import java.util.Scanner;
+
+public class Math_2839 {
+	public static void main(String[] args) {
+
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+
+		System.out.println(sugar(N));
+
+//		testMath_2893.testCase();
+	}
+
+	// 5부터 나누어서 나머지가 3 으로 나누어떨어지는지 확인
+	static int sugar(int N) {
+		int result = -1;
+		int count = 0;
+		int tmp5 = 0;
+		int tmp3 = 0;
+
+		if (N % 5 == 0) {
+//			System.out.println("5로 나누어짐");
+			result = N / 5;
+		} else {
+			count = N / 5;
+			for (int i = count; i >= 1; i--) {
+//				System.out.println("count - " + i);
+				tmp5 = N - (5 * i);
+//				System.out.println("tmp5 - " + tmp5);
+				tmp3 = tmp5 % 3;
+//				System.out.println("tmp3 - " + tmp3);
+				if (tmp3 == 0) {
+//					System.out.println(i + " " + tmp5 + " " + (tmp5 / 3));
+					result = i + (tmp5 / 3);
+					return result;
+				}
+			}
+			if (N % 3 == 0) {
+//				System.out.println("3으로 나누어짐");
+				result = N / 3;
+			}
+		}
+		return result;
+	}
+}
+
+// 테스트 코드
+class testMath_2893 {
+	static void testCase() {
+		int N = (int) (Math.random() * 50 + 1);
+
+		System.out.println("입력값: " + N);
+		System.out.println("출력값: " + Math_2839.sugar(N));
+
+	}
+}
